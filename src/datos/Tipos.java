7460c423d91d60ca886ebb63ad72089cb091c9de
JCJMA1994 <201214026@uns.edu.pe>
2022-06-21 18:39:34 -0500

    Start

---
+++ src/datos/Tipos.java
@@ -0,0 +1,15 @@
+package datos;
+
+public class Tipos {
+    public static void main(String[] args) {
+
+        int numberInt = 10;
+        long numberLong = 100;
+        double numberDouble = 1000.1;
+        boolean estado = true;
+        String nombre = "Jose Carlos Joao";
+        char sexo = 'M';
+
+
+    }
+}
