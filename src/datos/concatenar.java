c0ee1ddada31dd93a2cf52ee40e5f0ff3533a5e1
JCJMA1994 <201214026@uns.edu.pe>
2022-06-29 18:55:16 -0500

    Concatenar

---
+++ src/datos/concatenar.java
@@ -0,0 +1,9 @@
+package datos;
+
+public class concatenar {
+    public static void main(String[] args) {
+        String[] nombres = {"Jose", "Miguel", "Alfonso"};
+
+        System.out.println(" ".join(",", nombres));
+    }
+}
